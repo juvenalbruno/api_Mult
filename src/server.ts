e5d8597c872f8bfec2a3497c98c9e2766d1cdcc7
juvenalbruno <bruno.andrade178@hotmail.com>
2021-07-05 14:33:17 -0300

    20210705_1433

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { router } from './router';
+
+const HOST = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error)
+        return res.status(400).json({ error: err.message });
+    
+    return res.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+});
+
+app.listen(HOST, () => console.log(`>>> Server is running in http://localhost:${HOST}/`))
